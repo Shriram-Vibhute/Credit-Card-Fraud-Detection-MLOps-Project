5cb4a6aa95e5f689356b0bc60764d342d3e79127
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2025-01-13 16:59:21 +0530

    modified .gitignore file, dvc.lock, params. Added MLFlow experiment tracking and model registory pipeline

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,9 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pathlib
+from pathlib import Path
 import joblib
+import yaml
+import seaborn as sns
 
 from dvclive import Live
+import mlflow
+from mlflow.models import infer_signature
 
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix, precision_recall_curve, ConfusionMatrixDisplay, RocCurveDisplay
@@ -27,7 +32,7 @@ def load_model(model_path: str):
     model = joblib.load(model_path + "/model.joblib")
     return model
 
-def evaluate(model, X, y, live, split, save_path):
+def evaluate(model, X, y, live, split, dvc_save_path, mlflow_save_path):
     # Predictions
     prediction_by_class = model.predict_proba(X)
     predictions = prediction_by_class[:, 1] # Fetching the probability of fraud(Yes) credit cards
@@ -45,17 +50,65 @@ def evaluate(model, X, y, live, split, save_path):
     live.summary['avg_prec'][split] = avg_prec
     live.summary['roc_auc'][split] = roc_auc
 
+    # MLFlow metrics logging
+    mlflow.log_metric("average_precision_score", avg_prec)
+    mlflow.log_metric("roc_auc_score", roc_auc)
+
     # Logging Plots
+
+    # DVC logging - ROC curve
     live.log_sklearn_plot(
         "roc", y, predictions, name = f"roc/{split}"
     )
+    # MLFlow logging - ROC curve
+    fpr, tpr, _ = metrics.roc_curve(y, predictions)
+    plt.figure()
+    plt.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc:.2f})')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'ROC Curve - {split}')
+    plt.legend(loc="lower right")
+    roc_plot_path = f"{mlflow_save_path}/{split}/roc_curve.png"
+    pathlib.Path(f"{mlflow_save_path}/{split}").mkdir(parents=True, exist_ok=True)
+    plt.savefig(roc_plot_path)
+    plt.close()
+    mlflow.log_artifact(roc_plot_path)
+
+    # DVC Logging - Precision Recall Curve
     live.log_sklearn_plot(
         "precision_recall", y, predictions, name=f"prc/{split}", drop_intermediate = True,
-        # Threshold: The probability value above which a sample is classified as the positive class. For example, if the threshold is 0.95, only samples with predicted probabilities greater than or equal to 0.95 are classified as positive. (in test.json)
     )
+
+    # MLFlow logging - Precision Recall Curve
+    precision, recall, _ = metrics.precision_recall_curve(y, predictions)
+    plt.figure()
+    plt.plot(recall, precision, label=f'Precision-Recall curve (area = {avg_prec:.2f})')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title(f'Precision-Recall Curve - {split}')
+    plt.legend(loc="lower left")
+    prc_plot_path = f"{mlflow_save_path}/{split}/prc_curve.png"
+
+    plt.savefig(prc_plot_path)
+    plt.close()
+    mlflow.log_artifact(prc_plot_path)
+
+    # DVC Logging - Confusion Matrix
     live.log_sklearn_plot(
         "confusion_matrix", y, prediction_by_class.argmax(-1), name=f"cm/{split}"
     )
+
+    # MLFlow Logging - Confusion Matrix
+    cm = confusion_matrix(y, prediction_by_class.argmax(-1), labels=model.classes_)
+    plt.figure()
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=model.classes_, yticklabels=model.classes_)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title(f'Confusion Matrix - {split}')
+    cm_plot_path = f"{mlflow_save_path}/{split}/confusion_matrix.png"
+    plt.savefig(cm_plot_path)
+    plt.close()
+    mlflow.log_artifact(cm_plot_path)
 
 def save_plots(live, model, feature_names):
     fig, axes = plt.subplots(dpi=100)
@@ -79,9 +132,17 @@ def main():
     # Model path
     model_path = home_dir.as_posix() + "/models"
 
-    # Output path - Where all the plots and matrics are going to be sotred - dvclive
+    # Output path - Where all the plots and metrics are going to be stored - dvclive
     output_path = home_dir.as_posix() + '/dvclive'
     pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+
+    # Output path - MLFlow Artifacts
+    mlflow_output_path = home_dir.as_posix() + '/mlexps'
+    pathlib.Path(mlflow_output_path).mkdir(parents=True, exist_ok=True)
+
+    # MLFlow model saving path
+    mlflow_model_output_path = home_dir.as_posix() + '/mlflow_model'
+    pathlib.Path(mlflow_model_output_path).mkdir(parents=True, exist_ok=True)
 
     # Loading data and model
     X_train, X_test, y_train, y_test = load_data(data_path = input_path)
@@ -90,30 +151,63 @@ def main():
     # Features list
     feature_names = X_train.columns.to_list()
 
+    # Parameters path
+    params_path = home_dir.as_posix() + "/params.yaml"
+    params = yaml.safe_load(open(params_path, mode = 'r'))
+    params_dict = {}
+    for topic in params:
+        for each_param in params[topic]:
+            params_dict[f"{topic}_{each_param}"] = params[topic][each_param]
+    
+    # MLFlow setup
+    mlflow.set_tracking_uri(uri="http://localhost:8080")
+
+    # Create a new MLflow Experiment
+    mlflow.set_experiment("RandomForests Experiments")
+
     # Evaluation
     with Live(output_path, dvcyaml = False) as live:
-        # Train
-        evaluate(
-            model = model,
-            X = X_train,
-            y = y_train,
-            live = live,
-            split = "train",
-            save_path = output_path
-        )
+        with mlflow.start_run():
+            # Logging Parameters
+            mlflow.log_params(params_dict)
 
-        # Test
-        evaluate(
-            model = model,
-            X = X_test,
-            y = y_test,
-            live = live,
-            split = "test",
-            save_path = output_path
-        )
+            # Train
+            evaluate(
+                model = model,
+                X = X_train,
+                y = y_train,
+                live = live,
+                split = "train",
+                dvc_save_path = output_path,
+                mlflow_save_path = mlflow_output_path,
+            )
 
-        # Saving important plots - feature_importance_plots
-        save_plots(live, model, feature_names)
+            # Test
+            evaluate(
+                model = model,
+                X = X_test,
+                y = y_test,
+                live = live,
+                split = "test",
+                dvc_save_path = output_path,
+                mlflow_save_path = mlflow_output_path,
+            )
+
+            # Saving important plots - feature_importance_plots
+            save_plots(live, model, feature_names)
+
+        # Logging Model
+        # Infer the model signature
+        signature = infer_signature(X_train, model.predict(X_train))
+
+        # Log the model
+        model_info = mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="models",
+            signature=signature,
+            input_example=X_train,
+            registered_model_name="RandomForest Classifier",
+        )
 
 if __name__ == "__main__":
     main()
